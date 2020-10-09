dc578d57cd6ee5e0b0fc6228600cb43d81c65063
Ridge  Khan <ridgekhan@Ridges-MBP.attlocal.net>
2020-10-09 02:56:24 -0500

    structure and working

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Steak and Cheese', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Avocado', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Swiss', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
---
+++ public/js/burgers.js
@@ -0,0 +1,60 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".change-devoured").on("click", function(event) {
+      var id = $(this).data("id");
+      
+  
+      var newDevoured = {
+        devoured:1 
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newDevoured
+      }).then(
+        function() {
+          console.log("eaten");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurgers = {
+        name: $("#burgerName").val().trim(),
+    
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurgers
+      }).then(
+        function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".delete-cat").on("click", function(event) {
+      var id = $(this).data("id");
+  
+      // Send the DELETE request.
+      $.ajax("/api/cats/" + id, {
+        type: "DELETE"
+      }).then(
+        function() {
+          console.log("deleted cat", id);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
